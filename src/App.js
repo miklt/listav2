99308f4b474e81d9d795fea9fe64c53243aaff91
Michelet del carpio chavez <midelcax@hotmail.com>
2019-10-03 12:32:12 -0300

    Edição do css inicial.

--- src/App.js
+++ src/App.js
@@ -9,15 +9,30 @@ const Titulo = () => (
 )
 const Formulario = () => (
   <div className="formulario">
-  <input type='text' className="entradaProduto"></input>
-  <buton className="botaoAdicionar">+</buton>
+  <input placeholder="digite o produto e 
+  a quantidade" type='text' className="entradaProduto"></input>
+  <button className="botaoAdicionar">+</button>
   </div>
 )
-const Lista = () => (
-  <div className="lista"></div>
+const ProdutoEPreco = (props) => (
+  <div className='produtoEPreco'>
+    <div className='produto'>{props.nomeProduto}</div>
+    <div className='preco'>{props.precoProduto}</div>
+  </div>
+  
 )
+const Lista = () => (
+  <div className="lista">
+    <ProdutoEPreco nomeProduto='1 L Leite' precoProduto='R$ 2,80'/>
+    <ProdutoEPreco nomeProduto='1 cx bombons' precoProduto='R$ 8,20'/>
+    <ProdutoEPreco nomeProduto='1 lt óleo' precoProduto='R$ 3,40'/>
+
+
+  </div>
+)
+
 const Procurar = () => (
-  <div className="procurar"></div>
+  <button className="procurar">Procurar</button>
 )
 
 const App = () => (
