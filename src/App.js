ce528013cc9871d51b817d48d8b0b0b70f6563f7
Michelet del carpio chavez <midelcax@hotmail.com>
2019-10-03 13:57:12 -0300

    refatoração 1

--- src/App.js
+++ src/App.js
@@ -2,19 +2,26 @@ import React from 'react';
 
 import './App.css';
 import {ListaProdutosEPrecos} from './DadosIniciais.js'
-import {Titulo} from './Titulo'
-import {Formulario} from './Formulario'
-import {Lista} from './Lista'
-import {Procurar} from './Procurar'
+import {Titulo} from './componentes/Titulo'
+import {Formulario} from './componentes/Formulario'
+import {Lista} from './componentes/Lista'
+import {Procurar} from './componentes/Procurar'
 
 const App = () => (  
     <div className="conteiner">
       <Titulo/>
-      <Formulario/>
+      <Formulario onClick= {() => adicionarProdutos({"nome":"caneta"})}/>
       <Lista listaProdutos={ListaProdutosEPrecos}/>
       <Procurar/>
     </div>
   );
+function adicionarProdutos (produto) {  
+  // adiciona um produto
+  ListaProdutosEPrecos.push(produto)
+}
+function procurarPrecos (){
+  // Atualiza os preços dos produtos
+}
 
 
 export default App;
--- src/componentes/ProdutoEPreco.js
+++ src/componentes/ProdutoEPreco.js
@@ -2,7 +2,7 @@ import React from 'react';
 const ProdutoEPreco = (props) => (
     <div className='produtoEPreco'>
       <div className='produto'>{props.nomeProduto}</div>
-      <div className='preco'>{props.precoProduto}</div>
+      <div className='preco'>R${props.precoProduto}</div>
     </div>  
 )
 export {ProdutoEPreco}
