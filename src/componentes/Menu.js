58c64b5d3d30d46fc3c6c80c89bbd2275834c2d9
michelet <michelet@usp.br>
2019-10-24 12:01:04 -0300

    menu lateral superior

--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -3,7 +3,7 @@ import {
     Link    
   } from "react-router-dom";
 import '../App.css'
-const Menu = () => <div  className="menu">
+const Menu = () => <div className="menu">
     <nav >
         <ul>
         <li>
