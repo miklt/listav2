175d1f836c9c9f6d92d9e42833169cc4e8a963b3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-08 13:13:39 +0000

    Deploy miklt/listav2 to github.com/miklt/listav2.git:gh-pages

---
+++ precache-manifest.5e751980f62663e9ed0661e51eeb20cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d778b0d16efcbc7de39aa8542380ca0",
+    "url": "/listav2/index.html"
+  },
+  {
+    "revision": "68864656005bc4b3f6b2",
+    "url": "/listav2/static/css/main.993815cb.chunk.css"
+  },
+  {
+    "revision": "2a366121ba8c66e3dc65",
+    "url": "/listav2/static/js/2.c22195ba.chunk.js"
+  },
+  {
+    "revision": "68864656005bc4b3f6b2",
+    "url": "/listav2/static/js/main.3762656c.chunk.js"
+  },
+  {
+    "revision": "b60029a29f8150e05ab3",
+    "url": "/listav2/static/js/runtime-main.51b06a5a.js"
+  },
+  {
+    "revision": "c5507bb2a129e45b7aa98d52932bda06",
+    "url": "/listav2/static/media/background-mercado.c5507bb2.jpg"
+  }
+]);
